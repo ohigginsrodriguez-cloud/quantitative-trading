8a90b2c1e6d4d60040a47b2043638cf9f5464af7
O'Higgins Rodriguez Hernandez <ohigginsrodriguez@gmail.com>
2026-05-15 21:38:10 -0600

    feat: delete some unnecessary  scripts

--- bot_production/strategies/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import Strategy
-from .volatility import VolatilityStrategy
-
-__all__ = {'Strategy', 'VolatilityStrategy'}
--- bot_production/strategies/volatility_config.py
+++
@@ -1,8 +0,0 @@
-STRATEGY_PARAMS = {
-    'threshold': 2.0,
-    'window': 30,
-    'sl_pct': 0.015,
-    'tp_pct': 0.04
-}
-
-print('VOLATILITY CONFIG CARGADA')
